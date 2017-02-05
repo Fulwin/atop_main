f0ba84a6ff2cbf05e06db33b31f8643116f83b20
wangyue <18510209803@163.com>
2017-02-05 19:33:14 +1100

    u

--- resources/views/pages/downloads.blade.php
+++ resources/views/pages/downloads.blade.php
@@ -36,26 +36,5 @@
                 </div>
             </section>
         </div>
-
-        <div style="margin:0 auto; width:1200px; padding:25px;">
-            <div class="newscate">
-                <ul>
-                    <li class="catename">
-                        Solution
-                    </li>
-                    <li class="cateposition">Your current location is：<a href="{{ url('/') }}">Home</a> >
-                        <a href="">Solution</a>&nbsp; >&nbsp;
-                    </li>
-                </ul>
-            </div>
-            <div class="textcontentt2" style="width:1200px;"></div>
-        </div>
-        <article class="wrap" style="margin:0 auto; width:1200px; padding:25px;">
-            <section class="sec content2" style="position: relative;">
-                @if(isset($tech) && $tech)
-                    {!! $tech->Down_Content !!}
-                @endif
-            </section>
-        </article>
     </div>
 @stop
