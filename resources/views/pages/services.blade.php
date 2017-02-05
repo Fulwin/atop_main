731b786c2204c91e7749cbedb2d5fa4fda3e1a3c
wangyue <18510209803@163.com>
2017-02-05 16:59:12 +1100

    u

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -9,9 +9,9 @@
         @foreach($techs as $item)
             <section class="sec content2" style="position: relative;">
                 <div>
-                    <img id="dsImg" width="100%" src="{{ asset($item->BaseInfo_Image) }}">
+                    <img id="dsImg" width="100%" src="{{ asset($item->Down_Image) }}">
                     <a style="width: 7%;height: 25px;position: absolute;bottom: 5%;left: 50%;margin-left: -4%;border-radius:5px; background:#fff;box-shadow:0 0 5px #ccc; font-size:14px; text-align:center;"
-                       href="{{ url('/support/'.$item->BaseInfo_Id) }}" >More</a>
+                       href="{{ url('/support/'.$item->Down_Id) }}" >More</a>
                 </div>
             </section>
         @endforeach
