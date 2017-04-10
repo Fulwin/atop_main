774615f54a1b689bf8105213544ff3b4211c50f1
wangyue <18510209803@163.com>
2017-04-10 16:41:48 +1000

    Update news list page banner

--- resources/views/reusable_elements/desktop/banner.blade.php
+++ resources/views/reusable_elements/desktop/banner.blade.php
@@ -1,3 +1,3 @@
 <div class="bannarspicculture"
-     style="background:url('{{ $upload_files_prefix }}Upload/catebannar/news_banner-18394414129.jpg') center top no-repeat;">
+     style="background:url('{{ asset('/page_banner/news-1680.jpg') }}') center top no-repeat;height: 389px;">
 </div>
