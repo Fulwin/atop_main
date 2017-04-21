ba60579433a008ec271ea0ba5830bddb315ee57f
wangyue <18510209803@163.com>
2017-04-21 15:46:59 +1000

    u

--- resources/views/pages/solutions.blade.php
+++ resources/views/pages/solutions.blade.php
@@ -39,6 +39,7 @@
               </div>
           @endforeach
         </div>
+        @if(session('lang')=='EN')
         <div class="solutions-row solutions-extra-row">
           <div class="extra">
             <h3>ABOUT US</h3>
@@ -90,5 +91,59 @@ cloud computing.
             </div>
           </div>
         </div>
+        @else
+        <div class="solutions-row solutions-extra-row">
+          <div class="extra">
+            <h3>关于我们</h3>
+            <p class="subtitle">
+                坚守与创新
+            </p>
+            <div class="the-box">
+              <div class="box-icon">
+                <span class="icon-about-us"></span>
+              </div>
+              <div class="box-content">
+                  <p>
+                      华拓光通信是行业领先的光模块制造商，在光学元件、模块化产品和子系统的研发、生产和销售方面有着十分丰富的经验
+                  </p>
+              </div>
+            </div>
+          </div>
+          <div class="extra">
+            <h3>为什么选择我们</h3>
+            <p class="subtitle">
+                品质与预算
+            </p>
+            <div class="the-box">
+              <div class="box-icon">
+                <span class="icon-why-us">
+                <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span>
+                </span>
+              </div>
+              <div class="box-content">
+                <p>
+                    华拓光通信是行业领先的光模块制造商。华拓专业的研究和生产能力是我们的主要优势之一，在高速光模块设计方面拥有多年的经验
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="extra">
+            <h3>产品
+            </h3>
+            <p class="subtitle">高品质
+            </p>
+            <div class="the-box">
+              <div class="box-icon" style="padding-top:10px;">
+                <span class="icon-products" style="font-size: 50px;"></span>
+              </div>
+              <div class="box-content">
+                <p>
+                    华拓提供全系列的光器件解决方案，包括数据通信，SDH, FTTx , 数据中心和云计算
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        @endif
     </div>
 @stop
